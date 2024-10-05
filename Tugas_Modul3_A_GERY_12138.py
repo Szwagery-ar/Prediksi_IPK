642c45815e49909067fed507cd54c3177a0adb71
Stinnery69 <220712138@students.uajy.ac.id>
2024-10-05 16:01:18 +0700

    Update Tugas_Modul3_A_GERY_12138.py

--- Tugas_Modul3_A_GERY_12138.py
+++ Tugas_Modul3_A_GERY_12138.py
@@ -71,8 +71,7 @@ if uploaded_file is not None:
     st.write("<h3 style='text-align: center; color: #007a66;'>Data yang diupload:</h3>", unsafe_allow_html=True)
     st.dataframe(input_data)
 
-    model_directory = r'C:\Tery\.Semester 5\ML\Model Suprevised Learning (Praktek)\Tugas3_A_12138'
-    model_path = os.path.join(model_directory, r"SVR_IPK_model.pkl")
+    model_path = "SVR_IPK_model.pkl"
 
     if os.path.exists(model_path):
         with open(model_path, 'rb') as f:
